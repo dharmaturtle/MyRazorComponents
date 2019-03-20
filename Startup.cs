2ac018682f8503c123411838cc490d5f2af1b682
Alex Nguyen <dharmaturtle@gmail.com>
2019-03-20 06:44:04 -0500

    following https://simpleinjector.readthedocs.io/en/latest/aspnetintegration.html why isn't obj in the default gitignore...

--- Startup.cs
+++ Startup.cs
@@ -6,13 +6,20 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MyRazorComponents.Components;
 using MyRazorComponents.Services;
+using SimpleInjector;
+using SimpleInjector.Integration.AspNetCore.Mvc;
+using SimpleInjector.Lifestyles;
 
 namespace MyRazorComponents {
   public class Startup {
+    private Container container = new Container();
+
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services) {
@@ -21,11 +28,29 @@ namespace MyRazorComponents {
 
       services.AddRazorComponents();
 
-      services.AddSingleton<WeatherForecastService>();
+      IntegrateSimpleInjector(services);
+    }
+
+    private void IntegrateSimpleInjector(IServiceCollection services) {
+      container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+      services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+      services.AddSingleton<IControllerActivator>(
+        new SimpleInjectorControllerActivator(container));
+      services.AddSingleton<IViewComponentActivator>(
+        new SimpleInjectorViewComponentActivator(container));
+
+      services.EnableSimpleInjectorCrossWiring(container);
+      services.UseSimpleInjectorAspNetRequestScoping(container);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+      InitializeContainer(app);
+
+      container.Verify();
+
       if (env.IsDevelopment()) {
         app.UseDeveloperExceptionPage();
       } else {
@@ -41,5 +66,17 @@ namespace MyRazorComponents {
         routes.MapComponentHub<App>("app");
       });
     }
+
+    private void InitializeContainer(IApplicationBuilder app) {
+      // Add application presentation components:
+      container.RegisterMvcControllers(app);
+      container.RegisterMvcViewComponents(app);
+
+      container.RegisterSingleton<WeatherForecastService>();
+
+      // Allow Simple Injector to resolve services from ASP.NET Core.
+      container.AutoCrossWireAspNetComponents(app);
+    }
+
   }
 }
